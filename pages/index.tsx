46782e7d913ea63d203435fa5c4a86c0a27513f6
Riccardo Andreatta <26876480+Riccardo-Andreatta@users.noreply.github.com>
2022-03-03 16:39:25 +0000

    Next.js blog app transformed in TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,63 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
+
+export default function Home({
+    allPostsData
+}: {
+    allPostsData: {
+        date: string
+        title: string
+        id: string
+    }[]
+}) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+                <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+                <meta name="google" content="notranslate" key="notranslate" />
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>
+                    Welcome to this blog.<br />
+                    I am Riccardo, I am passionate about Front End stuff.
+                    In this case I am building a <em>Next.js</em> blog application.</p>
+                <p>
+                    (This is a sample <em>Next.js</em> website - you can build a blog like this following the{' '}
+                    <a href="https://nextjs.org/learn" target="_blank">Next.js tutorial</a>).
+                </p>
+            </section>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,72 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Post({
+    postData
+}: {
+    postData: {
+        title: string
+        date: string
+        contentHtml: string
+    }
+}) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    )
+}
+
+/**
+ * Function "getStaticPaths"
+ * 
+ * @returns object that contains the array of known paths and a fallback
+ * 
+ * `paths` contains the array of known paths returned by `getAllPostIds()`, which
+ * include the params defined by `pages/posts/[id].js`.
+ * 
+ * If `fallback` is `false`, then any paths not returned by `getStaticPaths` will
+ * result in a 404 page.
+ * 
+ * If `fallback` is `true`, then the behavior of getStaticProps changes:
+ * - The paths returned from `getStaticPaths` will be rendered to HTML at build time.
+ * - The paths that have not been generated at build time will not result in a 404 page.
+ *   Instead, Next.js will serve a “fallback” version of the page on the first request
+ *   to such a path.
+ * - In the background, Next.js will statically generate the requested path. Subsequent
+ *   requests to the same path will serve the generated page, just like other pages
+ *   pre-rendered at build time.
+ * 
+ * If `fallback` is `blocking`, then new paths will be server-side rendered with
+ * `getStaticProps`, and cached for future requests so it only happens once per path.
+ */
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id as string)
+    return {
+        props: {
+            postData
+        }
+    }
+}
