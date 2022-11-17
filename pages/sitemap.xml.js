84b17cac7e14e20af49bd39ecdf6017f8ca4f890
Riccardo Andreatta <26876480+Riccardo-Andreatta@users.noreply.github.com>
2022-11-17 14:32:25 +0000

    auto generated sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,4 @@
-const EXTERNAL_DATA_URL = 'https://nextjs-riccardo.vercel.app/'
+const EXTERNAL_DATA_URL = 'https://nextjs-riccardo.vercel.app/';
 
 function generateSiteMap(posts) {
     return `<?xml version="1.0" encoding="UTF-8"?>
@@ -11,7 +11,7 @@ function generateSiteMap(posts) {
                     `
                 }).join('')}
             </urlset>
-    `
+    `;
 }
 
 function SiteMap() {
@@ -20,20 +20,20 @@ function SiteMap() {
 
 export async function getServerSideProps({ res }) {
     // We make an API call to gather the URLs for our site
-    const request = await fetch(EXTERNAL_DATA_URL)
-    const posts = await request.json()
+    const request = await fetch(EXTERNAL_DATA_URL);
+    const posts = await request.json();
 
     // We generate the XML sitemap with the posts data
-    const sitemap = generateSiteMap(posts)
+    const sitemap = generateSiteMap(posts);
 
-    res.setHeader('Content-Type', 'text/xml')
+    res.setHeader('Content-Type', 'text/xml');
     // we send the XML to the browser
-    res.write(sitemap)
-    res.end()
+    res.write(sitemap);
+    res.end();
 
     return {
         props: {}
     }
 }
 
-export default SiteMap
+export default SiteMap;
