456cbad0bebe0c68cdd9f02a798852973b082c6f
Riccardo Andreatta <26876480+Riccardo-Andreatta@users.noreply.github.com>
2022-11-18 17:18:32 +0000

    added Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/global.css';
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
 
 export default App;
