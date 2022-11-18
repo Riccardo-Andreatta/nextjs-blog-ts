a16f654d0696c5e93540e6e7682af414c266323a
Riccardo Andreatta <26876480+Riccardo-Andreatta@users.noreply.github.com>
2022-11-18 09:29:28 +0000

    error pages updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
 function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <Layout>
+            <Head>
+                <title>404 - Page Not Found</title>
+            </Head>
+
+            <h1>404 - Page Not Found</h1>
+        </Layout>
+    );
 }
 
 export default Custom404;
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
 export default function Custom500() {
-    return <h1>500 - Server-side error occurred</h1>
+    return (
+        <Layout>
+            <Head>
+                <title>500 - Server-side error occurred</title>
+            </Head>
+
+            <h1>500 - Server-side error occurred</h1>
+        </Layout>
+    );
 }
